bca35ad3678a222dd311a4dde78d357df7c3f38d
Claquettes <mathieu.ponton@etu.univ-lyon1.fr>
2024-06-08 10:00:23 +0200

    feat(vols): uses the volService to call the api to get the departures from the selected city

--- src/app/components/view-airfrance/view-airfrance.component.ts
+++ src/app/components/view-airfrance/view-airfrance.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FiltresComponent } from '../filtres/filtres.component';
 import { ListeVolsComponent } from '../liste-vols/liste-vols.component';
 import { ListePassagersComponent } from '../liste-passagers/liste-passagers.component';
+import { VolService} from "../../services/vol.service";
 
 @Component({
   selector: 'app-view-airfrance',
@@ -11,7 +12,21 @@ import { ListePassagersComponent } from '../liste-passagers/liste-passagers.comp
   styleUrls: ['./view-airfrance.component.scss']
 })
 export class ViewAirFranceComponent {
-  onFiltersApplied(filters: {airport: string, startDate: Date, endDate: Date}) {
-    console.log(filters);
+  constructor(private volService: VolService) {
   }
+
+  convertToSeconds(date: Date): number {
+    return Math.floor(date.getTime() / 1000);
+  }
+
+
+
+  onFiltersApplied(filters: {airport: string, startDate: Date, endDate: Date}) {
+  const startDateS = this.convertToSeconds(filters.startDate);
+  const endDateS = this.convertToSeconds(filters.endDate);
+  this.volService.getVolsDepart(filters.airport, startDateS, endDateS)
+    .subscribe(vols => {
+      console.log(vols);
+    });
+}
 }
